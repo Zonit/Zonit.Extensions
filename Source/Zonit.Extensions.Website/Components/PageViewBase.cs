cf6f601c2cc13800d2c1fe00db09bbfd79951c5b
Dawid Haładus <alerinpoland@gmail.com>
2025-05-25 22:15:02 +0200

    Zaktualizowano wersję pakietu i zmieniono logikę stanu
    W pliku `Directory.Packages.props` zaktualizowano wersję zestawu z `0.1.0` na `0.1.1`.
    W pliku `PageViewBase.cs` usunięto wywołanie metody `await PersistState();` w bloku `try`, co może sugerować, że stan nie jest już zapisywany po załadowaniu modelu.

--- Source/Zonit.Extensions.Website/Components/PageViewBase.cs
+++ Source/Zonit.Extensions.Website/Components/PageViewBase.cs
@@ -68,7 +68,6 @@ public class PageViewBase<T> : ExtensionsBase
         try
         {
             Model = await LoadAsync();
-            await PersistState();
         }
         finally
         {
